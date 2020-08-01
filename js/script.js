b4dd62574e91a442dcd53fe23ff867ab4738a958
Nwachi Emmanuel <egreatness1@gmail.com>
2020-08-01 09:00:26 +0100

    feat: added button pagination

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+
+// Get all buttons with class="m-btn" in row3
+var m-btns = page.getElementsByClassName("m-btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < m-btns.length; i++) {
+  m-btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("btn-active");
+    current[0].className = current[0].className.replace(" btn-active", "");
+    this.className += " btn-active";
+  });
+}
